1047ec28ac0eaf189f117fe65c259a7f2318163c
DeveloperTesting305 <66563713+DeveloperTesting305@users.noreply.github.com>
2020-06-07 21:23:48 +0500

    Update PageSize.java

--- src/PageSize.java
+++ src/PageSize.java
@@ -2,7 +2,7 @@ public interface PageSize {
 
     Double ppi = Double.valueOf(72);
     ///**** UPDATED FROM OTHER SIDE
-
+    ///*** PULL REQUEST
     Double width = Double.valueOf(8.268) * ppi;
     Double height = Double.valueOf(11.693) * ppi;
 
